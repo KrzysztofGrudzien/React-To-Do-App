63f812003bd9b19b198ca033bdb1fd76be7ad332
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-11-24 00:42:06 +0100

    create a ToDoItem component as single a task to do

---
+++ src/components/ToDoItem/ToDoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ToDoItem = () => {
+  return (
+    <li>
+      Write more code.<button>complete</button>
+      <button>delete</button>
+    </li>
+  );
+};
+
+export default ToDoItem;
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ToDoItem from "../ToDoItem/ToDoItem";
 
 const ToDoList = () => {
   return (
     <div className="todo-wrapper">
-      <ul className="todo-list">My ToDoList</ul>
+      <ul className="todo-list">
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+      </ul>
     </div>
   );
 };
