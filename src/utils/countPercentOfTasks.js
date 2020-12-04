dbdc85c87f1cc2f927fc62ba345ffdc5847daa61
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-04 01:55:11 +0100

    fix return value

--- src/utils/countPercentOfTasks.js
+++ src/utils/countPercentOfTasks.js
@@ -1,7 +1,12 @@
 const countPercentOfTasks = (completed, tasks) => {
   const completedTask = parseInt(completed);
   const allTasks = parseInt(tasks);
-  return `${Math.round((completedTask / allTasks) * 100)}%`;
+  let percent = Math.round((completedTask / allTasks) * 100);
+
+  if (completedTask < 1) {
+    return `${0}%`;
+  }
+  return `${percent}%`;
 };
 
 export default countPercentOfTasks;
