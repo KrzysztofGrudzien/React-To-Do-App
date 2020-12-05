5c9dc3c5680cbc472aa9c0829204423244dada5c
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-05 01:07:04 +0100

    create  styled-component based on h3 tag matching dynamic title and color for choosed category

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -2,11 +2,50 @@ import React from "react";
 import ToDoItem from "../ToDoItem/ToDoItem";
 import "./ToDoList.scss";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const ToDoList = ({ toDoLists, setToDoLists, filteredToDoLists }) => {
+const TitleToDoList = styled.h3`
+  background-color: ${props =>
+    props.web
+      ? "#24c770"
+      : props.eng
+      ? "#e11bda"
+      : props.vr
+      ? "#d2d820"
+      : props.mark
+      ? "#ff3e6a"
+      : props.home
+      ? "#ec7a17"
+      : "#4c74f8"};
+  color: #f9feff;
+  font-size: 2rem;
+  font-weight: 600;
+  height: 70px;
+  line-height: 7rem;
+  margin-top: 50px;
+  text-transform: uppercase;
+  text-align: center;
+  width: 100%;
+`;
+
+const ToDoList = ({ toDoLists, setToDoLists, filteredToDoLists, category }) => {
   return (
     <div className="todo-list-wrapper">
-      <h3 className="todo-list-title">All tasks</h3>
+      {category === "webdesign" ? (
+        <TitleToDoList web>Webdesign</TitleToDoList>
+      ) : category === "vr" ? (
+        <TitleToDoList vr>Vr / 3D Development</TitleToDoList>
+      ) : category === "english" ? (
+        <TitleToDoList eng>English</TitleToDoList>
+      ) : category === "marketing" ? (
+        <TitleToDoList mark>Marketing</TitleToDoList>
+      ) : category === "home" ? (
+        <TitleToDoList home>Marketing</TitleToDoList>
+      ) : category === "development" ? (
+        <TitleToDoList dev>Development</TitleToDoList>
+      ) : (
+        <TitleToDoList>All tasks</TitleToDoList>
+      )}
       <ul className="todo-list">
         {filteredToDoLists.map(toDoList => (
           <ToDoItem
@@ -28,7 +67,9 @@ const ToDoList = ({ toDoLists, setToDoLists, filteredToDoLists }) => {
 
 ToDoList.propTypes = {
   toDoLists: PropTypes.array.isRequired,
-  setToDoLists: PropTypes.func.isRequired
+  setToDoLists: PropTypes.func.isRequired,
+  filteredToDoLists: PropTypes.array.isRequired,
+  category: PropTypes.string.isRequired
 };
 
 export default ToDoList;
