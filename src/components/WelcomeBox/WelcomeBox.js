f452d7f99c4cc0ed9e06a53d0128c508a44bd857
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-20 00:04:34 +0100

    create a welcome box component and style it

---
+++ src/components/WelcomeBox/WelcomeBox.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ImgAvatar from "../../assets/images/author@2x.jpg";
+import "./WelcomeBox.scss";
+
+const WelcomeBox = () => {
+  return (
+    <div className="welcome-box-wrapper">
+      <div className="welcome-box">
+        <h2 className="welcome-box-title">
+          <b>Hi, Krzysztof Grudzień</b>
+        </h2>
+        <p className="welcome-box-email">krzysztof.grudzien.fed@gmail.com</p>
+      </div>
+      <img src={ImgAvatar} alt="author" className="welcome-box-avatar" />
+    </div>
+  );
+};
+
+export default WelcomeBox;
