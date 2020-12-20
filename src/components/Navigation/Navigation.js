297a72c69201450f779fbbd4691efe84a973e3fb
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-20 02:54:02 +0100

    install react router

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,30 +1,45 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./Navigation.scss";
 
 const Navigation = () => {
   return (
-    <nav className="nav">
-      <ul className="nav-list">
-        <li className="nav-list-item">
-          <i class="fas fa-columns"></i>
-        </li>
-        <li className="nav-list-item">
-          <i class="fas fa-folder-open"></i>
-        </li>
-        <li className="nav-list-item">
-          <i class="fas fa-user-plus"></i>
-        </li>
-        <li className="nav-list-item">
-          <i class="fas fa-tasks"></i>
-        </li>
-        <li className="nav-list-item">
-          <i class="fas fa-chart-line"></i>
-        </li>
-        <li className="nav-list-item">
-          <i class="fas fa-user-cog"></i>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <Link to="/">
+              <i class="fas fa-columns"></i>
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/projects">
+              <i class="fas fa-folder-open"></i>
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/clients">
+              <i class="fas fa-user-plus"></i>
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/tasks">
+              <i class="fas fa-tasks"></i>
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/statistics">
+              <i class="fas fa-chart-line"></i>
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/settings">
+              <i class="fas fa-user-cog"></i>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
   );
 };
 
