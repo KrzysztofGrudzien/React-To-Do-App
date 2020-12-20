b63985f34bb942db7a3ec2e07d1e2abc78f269d7
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-20 02:16:50 +0100

    create new header component and style it

---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import UserLoginInfo from "../UserLoginInfo/UserLoginInfo";
+import "./Header.scss";
+
+const Header = ({ time, isActive, toDoLists, startTime, resetTime }) => {
+  let updateMinutes, updateSeconds, updateHours;
+
+  updateHours = Math.floor(time / 3600);
+  updateMinutes = Math.floor(time / 60) % 60;
+  updateSeconds = time % 60;
+
+  return (
+    <header className="app-header">
+      <h1 className="app-header-title">
+        TASK MANAGER <strong>for Everyone</strong>
+      </h1>
+      <div className="app-timer">
+        <span className="app-timer-time">
+          {updateHours < 10 ? `0${updateHours}` : updateHours} :{" "}
+          {updateMinutes < 10 ? `0${updateMinutes}` : updateMinutes} :{" "}
+          {updateSeconds < 10 ? `0${updateSeconds}` : updateSeconds}
+        </span>
+        <div className="app-timer-btn-group">
+          <button
+            onClick={startTime}
+            className={!isActive ? "app-timer-btn active" : "app-timer-btn"}
+            disabled={toDoLists.length < 1 ? true : false}
+          >
+            {isActive ? "start" : "pause"}
+          </button>
+          <button
+            onClick={resetTime}
+            className="app-timer-btn"
+            disabled={!isActive}
+          >
+            reset
+          </button>
+        </div>
+      </div>
+      <UserLoginInfo />
+    </header>
+  );
+};
+
+export default Header;
