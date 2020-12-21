1a2f23207d05f21725c9d7b2186cbe2184a8cbdd
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-22 00:47:40 +0100

    add new navigation link go to page displaying form

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,45 +1,48 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navigation.scss";
 
 const Navigation = () => {
   return (
-    <Router>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-list-item">
-            <Link to="/">
-              <i class="fas fa-columns"></i>
-            </Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/projects">
-              <i class="fas fa-folder-open"></i>
-            </Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/clients">
-              <i class="fas fa-user-plus"></i>
-            </Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/tasks">
-              <i class="fas fa-tasks"></i>
-            </Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/statistics">
-              <i class="fas fa-chart-line"></i>
-            </Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/settings">
-              <i class="fas fa-user-cog"></i>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </Router>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="nav-list-item">
+          <Link to="/">
+            <i class="fas fa-columns"></i>
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to="/projects">
+            <i class="fas fa-folder-plus"></i>
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to="/clients">
+            <i class="fas fa-user-plus"></i>
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to="/tasks">
+            <i class="fas fa-tasks"></i>
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to="/statistics">
+            <i class="fas fa-chart-line"></i>
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to="/settings">
+            <i class="fas fa-user-cog"></i>
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to="/add-task">
+            <i class="fas fa-plus-square"></i>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
