1e3556fc7143b5cd32444eb8107a5febac22905d
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-12-30 00:24:00 +0100

    add classes to form elements and style them

--- src/components/Form/AddProjectForm/AddProjecForm.js
+++ src/components/Form/AddProjectForm/AddProjecForm.js
@@ -1,33 +1,39 @@
 import React from "react";
+import "./AddProjectForm.scss";
 
 const AddProjectForm = () => {
   return (
-    <>
-      <h2>Add Project</h2>
+    <div className="form-container">
+      <h2>CREATE NEW PROJECT</h2>
       <form className="project-form">
         <div className="project-form-wrapper">
           <div className="project-input-wrapper">
-            <h3>Title</h3>
-            <input type="text" placeholder="Enter title of project" />
+            <h3 className="project-input-title">Title</h3>
+            <input
+              type="text"
+              placeholder="Enter title of project"
+              className="project-input"
+            />
           </div>
           <div className="project-input-wrapper">
-            <h3>Budget</h3>
-            <input type="number" min="0" />
+            <h3 className="project-input-title">Budget</h3>
+            <input type="number" min="0" className="project-input" />
           </div>
           <div className="project-textarea-wrapper">
-            <h3>Description</h3>
+            <h3 className="project-input-title">Description</h3>
             <textarea
               name=""
               id=""
               cols="30"
               rows="10"
               placeholder="Enter title of project"
+              className="project-textarea"
             ></textarea>
           </div>
-          <button>Add new project</button>
+          <button>Add project</button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
